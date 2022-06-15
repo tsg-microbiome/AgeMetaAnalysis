11b42b07d9e74d9b4a6dcf4883f64ef30bed0c03
tsg_microbiome <57810926+tsg-microbiome@users.noreply.github.com>
2022-06-16 02:00:43 +0530

    Update run_isc_analysis_stage2a_1.R

--- run_isc_analysis_stage2a_1.R
+++ run_isc_analysis_stage2a_1.R
@@ -1,3 +1,13 @@
+# The sub-pipeline works on the Irish Shotgun Cohorts (ISC) data repository and performs the following tasks:
+# Computes the sample-to-sample and taxa-to-taxa correlations between the clr-transformed abundances and the relative abundances of the different taxa at the species and genus level for the gut microbiomes of this dataset
+# Identifies a set of Highly Detected Species within this dataset.
+# Computes the association between the clr transformed abundances of this set of Highly Detected Species 
+# and the different microbiome summary statistic measures (four measures of uniqueness along with Shannon Diversity) using Robust Linear Regression models
+
+# The sub-pipeline requires the workspace isc_stage1_results.RData which is the output of the previous two sub-pipelines run_isc_analysis_stage1a.R, followed by
+# run_isc_analysis_stage1b.R, along with the isc_analysis_2021_Revision.RData which is the running workspace of the ISC dataset analysis (after the execution
+# of run_isc_analysis_stage1a.R and run_isc_analysis_stage1b.R)
+
 load("C:\\Projects\\ELDERMET\\NatureAgingRevision\\ISC\\isc_age_analysis.RData")
 load("C:\\Projects\\ELDERMET\\NatureAgingRevision\\isc_stage1_results.RData")
 load("C:\\Projects\\ELDERMET\\NatureAgingRevision\\ISC\\isc_Analysis_2021_Revision.RData")
